9e7283950f86ee82c14d09d0d53e59e475e4e2ce
Prince Kumar Yadav <princekumary271@gmail.com>
2025-12-02 13:25:38 +0530

    fixed status bar

--- app/src/main/java/com/app/healify/MainActivity.kt
+++ app/src/main/java/com/app/healify/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.app.healify.ui.theme.HealifyTheme
 import com.app.healify.views.PhoneHealthScreen
 
@@ -18,6 +12,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Ensure status bar is visible
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightStatusBars = true // Dark icons on light background
+        }
+
         setContent {
             HealifyTheme {
                 PhoneHealthScreen()
--- app/src/main/java/com/app/healify/utils/SafeArea.kt
+++ app/src/main/java/com/app/healify/utils/SafeArea.kt
@@ -4,7 +4,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,7 +15,8 @@ fun SafeArea(content : @Composable ()-> Unit) {
     Box(
         modifier = Modifier.fillMaxHeight().fillMaxWidth()
             .background(Color.White)
-            .systemBarsPadding()
+            .statusBarsPadding()
+            .navigationBarsPadding()
             .imePadding()
     ) {
         content()
